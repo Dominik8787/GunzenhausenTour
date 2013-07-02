7d34e13ac9528bf7a8ace8f6aa5be80953457bff
Sandra Taubmann <sandra.taubmann@fh-ansbach.de>
2013-07-02 16:26:36 +0200

    Barcode Scanner hinzugefügt + Einfache Seite zum Testen des Scanner. Evtl. noch Probleme wg. ZXing Library

---
+++ WebAppGun/assets/www/js/qr_scan.js
@@ -0,0 +1,16 @@
+function scan(){
+		document.getElementById("scan").addEventListener("click", function(){
+			//alert("Test successful");
+			window.plugins.barcodeScanner.scan( 
+				function(result) { 
+					alert("We got a barcode\n" +
+                  			"Result: " + result.text + "\n" +
+                  			"Format: " + result.format + "\n" +
+                  			"Cancelled: " + result.cancelled);
+				},
+				function(error) {
+					alert("Scanning failed: " + error)}
+			)
+		});
+		}
+window.onload=scan;
