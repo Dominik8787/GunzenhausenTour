ed8bb41bf9f3b4a928ab54f1b2a1c49ed55a911f
Sandra Taubmann <sandra.taubmann@fh-ansbach.de>
2013-07-10 17:40:59 +0200

    QR Code Scanner checkt Station und leitet ggf. weiter

--- WebAppGun/assets/www/js/qr_scan.js
+++ WebAppGun/assets/www/js/qr_scan.js
@@ -1,16 +1,22 @@
-function scan(){
-		document.getElementById("qrscan").addEventListener("click", function(){
-			//alert("Test successful");
-			window.plugins.barcodeScanner.scan( 
-				function(result) { 
-					alert("We got a barcode\n" +
-                  			"Result: " + result.text + "\n" +
-                  			"Format: " + result.format + "\n" +
-                  			"Cancelled: " + result.cancelled);
+function qrscan(pageID){
+	alert(pageID);
+	
+	//Barcode Scanner
+	window.plugins.barcodeScanner.scan( 
+				//Wenn Scann erfolgreich
+				function(result) {
+					//Holt Seitennamen aus der URL, indem er den ersten Teil der URL wegwirft (http://test.de/testID)
+				 	var res = result.text.substring(15); 
+					//alert("We got a barcode\n" +	"Result: " + result.text + "\n res: "+ res);
+                  	if(res == pageID) {      		
+                  		alert("Textmatch!");
+                  		window.location.href="index.html";		
+                  	} else {
+                  		alert("Oops! \n Da ist wohl was schief gegangen... \n Das ist nicht der richtige QR-Code!")
+                  	}
 				},
 				function(error) {
 					alert("Scanning failed: " + error)}
-			)
-		});
-		}
-window.onload=scan;
+			);
+	
+}
