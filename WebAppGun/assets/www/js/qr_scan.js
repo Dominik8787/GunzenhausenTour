8e0f57f9e2442cd737dc9d835bd59b1b733729bc
Sandra Taubmann <sandra.taubmann@fh-ansbach.de>
2013-07-12 18:42:26 +0200

    Station 4 (Färberturm) mit Abzweigungen, kleine Änderungen an den anderen Stationen

--- WebAppGun/assets/www/js/qr_scan.js
+++ WebAppGun/assets/www/js/qr_scan.js
@@ -5,12 +5,12 @@ function qrscan(pageID){
 	window.plugins.barcodeScanner.scan( 
 				//Wenn Scann erfolgreich
 				function(result) {
-					//Holt Seitennamen aus der URL, indem er den ersten Teil der URL wegwirft (http://test.de/testID)
-				 	var res = result.text.substring(15); 
+					//Holt Seitennamen aus der URL, indem er den ersten Teil der URL wegwirft (http://test.de/? ID)
+				 	var res = result.text.substring(16); 
 					//alert("We got a barcode\n" +	"Result: " + result.text + "\n res: "+ res);
                   	if(res == pageID) {      		
                   		alert("Textmatch!");
-                  		window.location.href="index.html";		
+                  		window.location.href=pageID;		
                   	} else {
                   		alert("Oops! \n Da ist wohl was schief gegangen... \n Das ist nicht der richtige QR-Code!")
                   	}
