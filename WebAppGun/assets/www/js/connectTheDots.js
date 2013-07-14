c253a6bc947202bcbe1c14559617d28496227891
Dominik Stein <dominik.stein@hs-ansbach.de>
2013-07-14 17:15:49 +0200

    connectTheDots fertig. hat weiterleitung, bild fadet am ende rein. nicht responsive wegen canvas

--- WebAppGun/assets/www/js/connectTheDots.js
+++ WebAppGun/assets/www/js/connectTheDots.js
@@ -1,102 +1,141 @@
 $(document).ready(function() {
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-context.fillStyle = 'black';
-context.strokeWidth = 5;
+	$("#WeiterButtonConnectTheDots").hide();
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+    var imageObj = new Image();
+    imageObj.src = 'img/storch.gif';
+	context.fillStyle = 'black';
+	context.strokeWidth = 5;
 
-var started = false;
-var coords = [];
-var clickCount = 0;
-var alertCount = 0;
-var tween = null;
-var oldPoint;
-var newPoints = [];
-var drawPoints = [];
+	var started = false;
+	var coords = [];
+	var clickCount = 0;
+	var alertCount = 0;
+	var tween = null;
+	var oldPoint;
+	var newPoints = [];
+	var drawPoints = [];
 
-var Punkt = {
-	x : 0,
-	y : 0
-};
-
-for ( var i = 0; i !== 16; i++) {
-
-	var p = Object.create(Punkt);
-	p.x = parseInt($("#punkt" + i).css("left")) + 5;
-	p.y = parseInt($("#punkt" + i).css("top")) + 5;
-	coords.push(p);
-
-}
-
-function clickHandler() {
-
-	var index = $(this).index();
-
-	$(this).addClass("active");
-	$("#punkt" + (index - 1)).removeClass("active");
-	$("#punkt" + (index + 1)).addClass("active");
-	// console.log("Index " + index);
-	// console.log("X-achse: " + coords[index].x);
-	drawPoints[clickCount] = coords[index];
-	clickCount++;
-	alertCount++;
-	if (alertCount === 20) {
-		alert("folge den rot leuchtenden Punkten!");
-		window.location.reload();
-	}
-	console.log(clickCount);
-	// console.log("drawPoints"+ drawPoints[1]);
-
-	if (!started) {
-		started = true;
-		oldPoint = coords[index];
-		console.log(oldPoint);
-	} else if (clickCount === 2) {
-		// drawPoints[0].x, drawPoints[0].y = drawPoints[1].x, drawPoints[1].y;
-		clickCount = 1;
-		newPoints = oldPoint;
-		drawLine(drawPoints);
-		// zuletzt angeklickter punkt = basispunkt;
-
-		// drawPoints = [];
-	}
-}
-
-$(".punkt").click(clickHandler);
-$("#punkt0").addClass("active");
-
-function drawLine(points) {
-	$(".punkt").off("click");
-
-	context.beginPath();
-	// context.moveTo(points[0].x, points[0].y);
-	context.moveTo(oldPoint.x, oldPoint.y);
-	var endPoint = {
-		x : oldPoint.x,
-		y : oldPoint.y
+	var Punkt = {
+		x : 0,
+		y : 0
 	};
 
-	tween = new TWEEN.Tween(endPoint).to({
-		x : points[1].x,
-		y : points[1].y
-	}, 1000).easing(TWEEN.Easing.Linear.None).onUpdate(function() {
+	for ( var i = 0; i !== 17; i++) {
 
-		context.lineTo(endPoint.x, endPoint.y);
-		context.closePath();
-		context.stroke();
+		var p = Object.create(Punkt);
+		p.x = parseInt($("#punkt" + i).css("left")) + 5;
+		p.y = parseInt($("#punkt" + i).css("top")) + 5;
+		coords.push(p);
+	}
 
-	}).start();
+	function clickHandler() {
+		//Check ob das Spiel beendet ist (punkt 16 ist aktiv und wird noch mal geklickt)
+		 if ($('#punkt16').is('.active') ) {
+			 	
+				 alert("weiterleiten!")
+				 $("#WeiterButtonConnectTheDots").show();
+				 context.globalAlpha = 0;
+				 var imgFadeInter = setInterval(function(){
+				// clearContext(); // a function that clears the canvas
+				 context.globalAlpha += 0.01;
+				 context.drawImage(imageObj, 10, 10, 320, 480);
+				 if(context.globalAlpha == 1){ 
+					 clearInterval(imgFadeInter); 
+				 }
+				 }, 16); // 16ms because jQuery says so 
+				 };
+				//window.location.href="02lab01.html#page02_10";
+			
 
-	tween.onComplete(function() {
-		$(".punkt").click(clickHandler);
-	});
-	oldPoint = points[1];
-}
+		var index = $(this).index();
 
-function animate() {
-	requestAnimationFrame(animate);
+		$(this).addClass("active");
+		$("#punkt" + (index - 1)).removeClass("active");
+		$("#punkt" + (index + 1)).addClass("active");
+		// console.log("Index " + index);
+		// console.log("X-achse: " + coords[index].x);
+		drawPoints[clickCount] = coords[index];
+		clickCount++;
+		alertCount++;
+		if (alertCount === 20) {
+			alert("folge den rot leuchtenden Punkten!");
+			window.location.reload();
+		}
+		console.log(clickCount);
+		// console.log("drawPoints"+ drawPoints[1]);
 
-	TWEEN.update();
-};
+		if (!started) {
+			started = true;
+			oldPoint = coords[index];
+			console.log(oldPoint);
+		} else if (clickCount === 2) {
+			// drawPoints[0].x, drawPoints[0].y = drawPoints[1].x,
+			// drawPoints[1].y;
+			clickCount = 1;
+			newPoints = oldPoint;
+			drawLine(drawPoints);
+			// zuletzt angeklickter punkt = basispunkt;
 
-animate();
+			// drawPoints = [];
+		}
+	}
+	
+	/*function endCheck() {
+		console.log("classCheck")
+		
+			alert("weiterleiten!")
+			window.location.href="02lab01.html#page02_10";
+		}; */
+		
+	$(".punkt").click(clickHandler);
+	//$("#punkt16").click(endCheck);
+	
+	/* $(".punkt").click(function(){
+   
+	}); */
+	
+	
+	$("#punkt0").addClass("active");
+	
+	
+	
+
+	function drawLine(points) {
+		$(".punkt").off("click");
+
+		context.beginPath();
+		// context.moveTo(points[0].x, points[0].y);
+		context.moveTo(oldPoint.x, oldPoint.y);
+		var endPoint = {
+			x : oldPoint.x,
+			y : oldPoint.y
+		};
+
+		tween = new TWEEN.Tween(endPoint).to({
+			x : points[1].x,
+			y : points[1].y
+		}, 1000).easing(TWEEN.Easing.Linear.None).onUpdate(function() {
+
+			context.lineTo(endPoint.x, endPoint.y);
+			context.closePath();
+			context.stroke();
+
+		}).start();
+
+		tween.onComplete(function() {
+			$(".punkt").click(clickHandler);
+		});
+		oldPoint = points[1];
+	}
+
+	function animate() {
+		requestAnimationFrame(animate);
+
+		TWEEN.update();
+	}
+	;
+
+	animate();
+
 });
